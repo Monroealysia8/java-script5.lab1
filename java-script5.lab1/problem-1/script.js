6f52b304907603bf16d0c54a8766cca56d78673a
Monroealysia8 <monroealysia8@gmail.com>
2025-02-22 23:05:32 -0600

    push all changed

---
+++ java-script5.lab1/problem-1/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function calculateCtoF(tempC) {
+  let c = Number(tempC);
+  let F = (c * 9) / 5 + 32;
+  return F;
+}
+calculateCtoF(5);
+
+let div = 
+
+document.write("The Temperature you entered in Fahrenheit is  ");
